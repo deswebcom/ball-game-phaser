46d9d1df765d068be2cafcc56b78cff6a12be71b
midesweb <malvarez@desarrolloweb.com>
2020-08-31 01:08:24 +0200

    Platform

--- game.js
+++ game.js
@@ -7,11 +7,30 @@ export class Game extends Phaser.Scene {
   preload() {
     this.load.image('background', 'images/background.png');
     this.load.image('gameover', 'images/gameover.png');
+    this.load.image('platform', 'images/platform.png');
   }
 
   create() {
     this.add.image(410, 250, 'background');
     this.gameoverImage = this.add.image(400, 90, 'gameover');
+    this.gameoverImage.visible = false;
+    
+    this.platform = this.physics.add.image(400, 460, 'platform')
+    this.platform.body.allowGravity = false;
+    
+    this.cursors = this.input.keyboard.createCursorKeys();
+  }
+
+  update() {
+    if (this.cursors.left.isDown) {
+      this.platform.setVelocityX(-500);
+    }
+    else if (this.cursors.right.isDown) {
+      this.platform.setVelocityX(500);
+    }
+    else {
+      this.platform.setVelocityX(0);
+    }
   }
 
 }
