6b4f335c61361d277ba015f701dcb6b2d73773bc
midesweb <malvarez@desarrolloweb.com>
2020-09-01 17:43:13 +0200

    change variable name

--- components/live-counter.js
+++ components/live-counter.js
@@ -7,9 +7,9 @@ export class LiveCounter {
 
   create() {
     let displacement = 60;
-    let finalPosition = 820 - ((this.lives - 1) * displacement);
+    let firstPosition = 820 - ((this.lives - 1) * displacement);
     for (let i = this.lives; i > 1 ; i--) {
-      let horizontalPosition = ((i - 2) * displacement) + finalPosition;
+      let horizontalPosition = ((i - 2) * displacement) + firstPosition;
       let currenDisplay = this.relatedScene.physics.add.image(horizontalPosition, 15, 'platform').setScale(0.5);
       this.liveDisplayers.push(currenDisplay);
     }
